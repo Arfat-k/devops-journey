d8f0d63f6bb60610ac43c0db7ac5153a0c77885e
ARFAT <affuarfatkhan92@gmail.com>
2026-04-28 10:10:03 +0530

    created backup script

---
+++ backup.sh
@@ -0,0 +1,9 @@
+echo " ========================="
+echo "      backup script       "
+echo "    starting backup       "
+mkdir -p backup
+cp myfirst.sh backup/
+cp systeminfo.sh backup/
+echo " backup created successfully "
+echo " files saved in backup folder "
+echo " ========================="
