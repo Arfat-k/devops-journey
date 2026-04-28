b7da84e2d6c93e69d832d1a272e484e533290059
ARFAT <affuarfatkhan92@gmail.com>
2026-04-28 09:34:00 +0530

    added system info script

---
+++ systeminfo.sh
@@ -0,0 +1,10 @@
+echo "=============================="
+echo "      SYSTEM INFORMATION      "
+echo "=============================="
+echo "Your Username is:"
+whoami
+echo "Today's Date is:"
+date
+echo "Your Current Location is:"
+pwd
+echo "=============================="
