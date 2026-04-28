4f5f8d2e9036e81229cb9c9901e795abf6c1e03b
ARFAT <affuarfatkhan92@gmail.com>
2026-04-28 11:28:04 +0530

    welcome script

---
+++ welcome.sh
@@ -0,0 +1,10 @@
+echo " ==================="
+echo "   welcome script "
+echo " ==================="
+echo "    my name is " 
+ whoami
+echo " current date and time "
+date
+echo " motivational message "
+echo " never give up "
+echo " ===================="
